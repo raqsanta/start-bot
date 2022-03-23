f53d4225f77ff08dbdf6fb1332d2ba91f319e992
vhisla <eahisla@gmail.com>
2022-03-23 18:00:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,35 @@ client.on('ready', () => {
 
 client.on('messageCreate', async interaction => {
 
-    if(interaction.content == '!ping'){
+  if (!interaction.content.startsWith('st!')) return
 
-        interaction.channel.send('testee')
+  const message = interaction.content.substring(3)
 
-    }
+  if (!message) return
+
+  if (message == 'avisos') {
+
+    interaction.channel.send('aqui serão enviados os avisos do site')
+
+  }
+
+  if (message == 'regras') {
+
+    interaction.channel.send('aqui serão enviadas as regras do site')
+
+  }
+
+  if (message == 'meet' || message == 'reunião' || message == 'palestra') {
+
+    interaction.channel.send('a reunião hoje será sobre, link da reunião no meet:')
+
+  }
+
+  if (message == 'board') {
+
+    interaction.channel.send('aqui estarão todas as tarefas e afazares pendentes:')
+
+  }
 
 })
 
