ea32da455f85f73fd1aac32cfb953c9e1c8c849a
vhisla <eahisla@gmail.com>
2022-03-24 10:18:13 -0300

    Add Web Scraping

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { Client, Intents, Message } = require('discord.js');
+const { Client, Intents, Message, MessageAttachment } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require('dotenv').config()
+
+const puppeteer = require('puppeteer');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -15,9 +17,10 @@ client.on('messageCreate', async interaction => {
 
   if (!message) return
 
-  if (message == 'avisos') {
+  if (message == 'help' || message == 'sobre' || message == 'ajuda') {
 
-    interaction.channel.send('aqui serão enviados os avisos do site')
+    interaction.reply('Algumas informações sobre o robô foram enviadas ao seu privado.')
+    interaction.author.send('teste')
 
   }
 
@@ -29,15 +32,57 @@ client.on('messageCreate', async interaction => {
 
   if (message == 'meet' || message == 'reunião' || message == 'palestra') {
 
-    interaction.channel.send('a reunião hoje será sobre, link da reunião no meet:')
+    interaction.channel.send('a reunião hoje será sobre, conheça o palestrante link da reunião no meet:')
 
   }
 
-  if (message == 'board') {
+  if (message == 'board' || message == 'avisos') {
 
     interaction.channel.send('aqui estarão todas as tarefas e afazares pendentes:')
 
   }
+
+  if (message == 'scrap') {
+
+    (async () => {
+
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto('https://academia.iphac.org.br/login/index.php');
+
+      const currentDate = Date.now()
+
+      //await page.screenshot({ path: './images/screenshot-' + currentDate + '.png' });
+
+      await page.evaluate( async (CPF, PASSWORD) => {
+
+        //id = username
+        //id = password
+        //id = loginbtn
+
+        document.getElementById('username').value = CPF
+        document.getElementById('password').value = PASSWORD
+
+      }, process.env.USER_CPF, process.env.USER_PASSWORD)
+
+      await page.click('[id="loginbtn"]')
+
+      await page.waitForNavigation();
+
+      await page.screenshot({ path: './images/screenshot-' + currentDate + '.png' });
+
+      await browser.close();
+
+      const attachment = new MessageAttachment('./images/screenshot-' + currentDate + '.png')
+
+      interaction.reply({ files: [attachment] })
+
+    })();
+
+
+  }
+
+
 
 })
 
